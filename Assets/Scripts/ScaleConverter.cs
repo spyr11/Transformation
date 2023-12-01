dd41cf6844939e777df9ea25691755ee706879b3
spyr11 <100491931+spyr11@users.noreply.github.com>
2023-12-01 18:26:33 +0300

    Update ScaleConverter.cs

--- Assets/Scripts/ScaleConverter.cs
+++ Assets/Scripts/ScaleConverter.cs
@@ -1,22 +1,18 @@
 using UnityEngine;
 
 public class ScaleConverter : MonoBehaviour
-{    [SerializeField] private float _speed;
+{
+    [SerializeField] private float _speed;
 
     private Vector3 _currentScale;
 
     private void Start()
     {
-        _currentScale = transform.localScale;        
+        _currentScale = transform.localScale;
     }
 
     private void Update()
     {
-        transform.localScale += _currentScale* _speed * Time.deltaTime;
-    }
-
-    private void FixedUpdate()
-    {
-        Debug.Log(transform.localScale.y);
+        transform.localScale += _currentScale * _speed * Time.deltaTime;
     }
 }
